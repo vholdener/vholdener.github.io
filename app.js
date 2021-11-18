ad0a3797727d0af9331e0cfcdc3332c6c5877612
vholdener <vikhold@sunrise.ch>
2021-11-18 20:26:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+const COSMO_DATA = [
+  { id: 1, temperatur: 18, sonne:2, niederschlag: 5, tag: 'montag' },
+  { id: 2, temperatur: 16, sonne:4, niederschlag: 5, tag: 'dienstag' },
+  { id: 3, temperatur: 10, sonne:8, niederschlag: 6, tag: 'mittwoch' },
+  { id: 4, temperatur: 18, sonne:9, niederschlag: 10, tag: 'donnerstag' },
+  { id: 5, temperatur: 20, sonne:1, niederschlag: 5, tag: 'freitag' }
+]
+
 d3.select('div')
   .selectAll('p')
-  .data([1, 2, 3])
+  .data([COSMO_DATA])
   .enter()
   .append('p')
-  .text(dta  => dta);
+  .text(dta  => dta.tag);
+
+// d3.select('div')
+//   .selectAll('p')
+//   .data([COSMO_DATA])
+//   .enter()
+//   .append('p')
+//   .text(dta  => dta.tag);
